6e0fa6e84570a9fe0dc8723a99f83ac0a3a97204
Daniel Escobar <daescobarp@unal.edu.co>
2018-10-07 21:56:21 -0500

    Added uniqueness validation to username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 200, minimum: 2}, format: { with: /\A[^0-9]+\z/}   
     validates :lastname, presence: true, length: {maximum: 200, minimum: 2}, format: { with: /\A[^0-9]+\z/}   
-    validates :email, presence:true, length: {maximum: 200, minimum: 5}, format: {with: /\A\S+@.+\.\S+\z/}
+    validates :email, presence: true, length: {maximum: 200, minimum: 5}, format: {with: /\A\S+@.+\.\S+\z/}
+    validates :username, presence: true, uniqueness: true, length: {maximum: 50, minimum: 2}, format: {with: /\A\S+\z/}
     validates :password, presence: true, length: {maximum: 250}
     validates :score, presence: false
     validates :talk_to_us, length: {maximum: 10000}
