90157e9380659017a8a9d64707f59ab7e6ae22d4
Laura <ldmolinav@unal.edu.co>
2018-11-08 04:05:35 -0500

    Fixed username in jwt

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,10 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
+  def create
+    @user = User.find_by(email: params[:auth][:email])
+    render json: {
+        jwt: auth_token.token,
+        username: @user.username
+      }, status: :created
+  end
 end
