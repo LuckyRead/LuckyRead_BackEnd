3ba17a28774d5eea95dd25b45b5441aac2a884ce
Daniel Escobar <daescobarp@unal.edu.co>
2018-11-16 14:18:51 -0500

    NiniFix

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,7 +6,7 @@ class TopicsController < ApplicationController
     @love = SubTopicsUser.where('user_id = ? and sub_topic_id = ?', current_user.id, params[:id])
     if !@love.nil?
       render json: {hurra: 'User loves this sub topic'}, status: :ok
-    else
+    elsif @love == []
       render json: {ups: 'User does not love this sub topic yet'}, status: :ok
     end
   end
