610742ffd4b586f64eacd12fd8c3c87ca112abc2
Cristhian <cgcontrerasm@unal.edu.co>
2018-12-01 19:26:53 -0500

    five fragments...

--- app/controllers/fragments_controller.rb
+++ app/controllers/fragments_controller.rb
@@ -252,7 +252,7 @@ class FragmentsController < ApplicationController
             score: @fragment.score,
             source: @fragment.source,
             base64_image: Photo.find(@fragment.photos_id).base64_image,
-            count_all => @percentage
+            count_all: @percentage
           }}
           arrayFiveFragments.push(hash1)
         end
