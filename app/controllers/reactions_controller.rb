a8aa75dbf054d456e2498ca0cefad7d1cdd4e776
Cristhian <cgcontrerasm@unal.edu.co>
2018-12-05 21:21:20 -0500

    reactions_to_freagments

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -30,16 +30,13 @@ class ReactionsController < ApplicationController
     render json: {Reactios_of_current_users_to_all_fragments: @reactions}, status: :ok
   end
 
-  def 
+  def numbers_of_likes
     @user = current_user
     @comments = Comment.find_by(user_id: @user.id)
-    @likes = 0
+    @likes
     @comments.each do |id|
       @likes = @likes + LikeComment.find_by(comments_id: id).group(:comments_id).count()
     end
-    
-
-
   end
   # PATCH/PUT /reactions/1
   def update
