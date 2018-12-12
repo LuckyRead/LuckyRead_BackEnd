7c7a7ff75e351252c5d6a4d1a364ed54a84a7d0c
Cristhian <cgcontrerasm@unal.edu.co>
2018-12-11 22:25:23 -0500

    All info user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,7 +118,7 @@ class UsersController < ApplicationController
 
   def all_info_user
     @username = params[:username]
-    @hash = User.where("users.username = ?", @username)
+    @hash = User.where("users.name = ?", @username)
     render json: @hash, status: :ok
   end
 
