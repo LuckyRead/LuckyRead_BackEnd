4484757ada822e48c1fce316eb736eb5e83af225
Cristhian <cgcontrerasm@unal.edu.co>
2018-12-11 22:06:33 -0500

    All info user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,7 +118,7 @@ class UsersController < ApplicationController
 
   def all_info_user
     @username = params[:username]
-    hash = User.find(@username)
+    hash = User.find_by(username: @username)
     render json: hash, status: :ok
   end
 
